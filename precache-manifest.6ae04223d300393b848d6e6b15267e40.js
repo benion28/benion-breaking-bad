abcaa549cb3650a228eb42fa15d60daec4f50867
Bernard Iorver <bernard.benion.iorver@hotmail.com>
2020-07-09 03:15:34 +0100

    Updates

---
+++ precache-manifest.6ae04223d300393b848d6e6b15267e40.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbae2d8ba4bbd7ca530103df116b9421",
+    "url": "/benion-breaking-bad/index.html"
+  },
+  {
+    "revision": "b656b312ab80431be8fb",
+    "url": "/benion-breaking-bad/static/css/main.a224737c.chunk.css"
+  },
+  {
+    "revision": "8a01c93dd579f02264c4",
+    "url": "/benion-breaking-bad/static/js/2.68fc3778.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/benion-breaking-bad/static/js/2.68fc3778.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b656b312ab80431be8fb",
+    "url": "/benion-breaking-bad/static/js/main.e68bc03c.chunk.js"
+  },
+  {
+    "revision": "1fb03a290915440f14ea",
+    "url": "/benion-breaking-bad/static/js/runtime-main.aa68e866.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/benion-breaking-bad/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/benion-breaking-bad/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/benion-breaking-bad/static/media/spinner.235b9b3e.gif"
+  }
+]);
